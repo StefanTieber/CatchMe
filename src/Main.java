a7540fcba22664f10a1991237d2e15d0f8d40365
Thomas Tieber <Thomas.Tieber@cloudflight.io>
2021-04-04 18:05:13 +0200

    generify size

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import utils.Position;
 
 public class Main {
     public static void main(String[] args) {
-//        Player catcher = new Player(new Position(200, 200));
-//        Player runner = new Player(new Position(300, 200));
-//        FrontendController frontendController = new FrontendController(catcher, runner);
+        Player catcher = new Player(new Position(0, 0));
+        Player runner = new Player(new Position(300, 200));
+        FrontendController frontendController = new FrontendController(500, 500, catcher, runner);
 
         System.out.println("Du Schneckli!");
     }
--- src/frontend/FrontendController.java
+++ src/frontend/FrontendController.java
@@ -7,20 +7,19 @@ import java.awt.*;
 
 public class FrontendController extends JPanel {
     private static final int PLAYER_SIZE = 20;
-    private static final int FRAME_WIDTH = 500;
-    private static final int FRAME_HEIGHT = 500;
+    public static final int BORDER_THICKNESS = 10;
 
     private JFrame frame;
 
     private Player catcher;
     private Player runner;
 
-    public FrontendController(Player catcher, Player runner) {
+    public FrontendController(int gamewidth, int gameheight, Player catcher, Player runner) {
         this.catcher = catcher;
         this.runner = runner;
 
         frame = new JFrame("Catch Me!");
-        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        frame.setSize(adjustForBorders(gamewidth), adjustForBorders(gameheight));
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
@@ -28,12 +27,31 @@ public class FrontendController extends JPanel {
         frame.setVisible(true);
     }
 
+    private int adjustForBorders(int size) {
+        return size + BORDER_THICKNESS * 2 + PLAYER_SIZE / 2;
+    }
+
+    private int adjustForBorder(int x) {
+        return x + BORDER_THICKNESS;
+    }
+
     @Override
     public void paint(Graphics g) {
-        g.clearRect(0, 0, this.getWidth(), this.getHeight());
-
+        clearScreen(g);
+        drawBorder((Graphics2D) g);
         drawCatcher(g);
         drawRunner(g);
+    }
+
+    private void clearScreen(Graphics g) {
+        g.clearRect(0, 0, this.getWidth(), this.getHeight());
+    }
+
+    private void drawBorder(Graphics2D g) {
+        Stroke oldStroke = g.getStroke();
+        g.setStroke(new BasicStroke(BORDER_THICKNESS));
+        g.drawRect(BORDER_THICKNESS / 2, BORDER_THICKNESS / 2, this.getWidth() - 1 - BORDER_THICKNESS / 2, this.getHeight() - 1 - BORDER_THICKNESS / 2);
+        g.setStroke(oldStroke);
     }
 
     private void drawCatcher(Graphics g) {
@@ -46,6 +64,6 @@ public class FrontendController extends JPanel {
 
     private void drawPlayer(Graphics g, Player player, Color red) {
         g.setColor(red);
-        g.fillOval((int) player.position.x, (int) player.position.y, PLAYER_SIZE, PLAYER_SIZE);
+        g.fillOval(adjustForBorder((int) player.position.x), adjustForBorder((int) player.position.y), PLAYER_SIZE, PLAYER_SIZE);
     }
 }
