ca01422c7165bf7bcf55e0e5889d3099bf66ff48
StefanTieber <stefantieber23@gmail.com>
2021-04-04 13:43:51 +0200

    Add Vector

--- src/utils/Vector.java
+++ src/utils/Vector.java
@@ -1,4 +1,25 @@
 package utils;
 
 public class Vector {
+    public float x;
+    public float y;
+
+    public Vector(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getLength() {
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    public float getDirection() {
+        return (float) Math.atan2(y, x);
+    }
+
+    public void setLengthOne() {
+        float length = getLength();
+        x /= length;
+        y /= length;
+    }
 }
