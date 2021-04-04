60aa93423aae13f714500a7ce0634c0f8f6e5a3c
StefanTieber <stefantieber23@gmail.com>
2021-04-04 17:04:00 +0200

    Add GameController Constructor

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -5,14 +5,27 @@ import utils.Position;
 import utils.Vector;
 
 public class GameController {
-    Player runner;
-    Player catcher;
+    public final float MAX_SPEED = 30;
 
-    Position runnerPosition;
-    Position catcherPosition;
+    private Player runner;
+    private Player catcher;
 
-    Vector runnerSpeed;
-    Vector catcherSpeed;
+    private Position runnerPosition;
+    private Position catcherPosition;
+
+    private Vector runnerSpeed;
+    private Vector catcherSpeed;
+
+    public GameController(Player runner, Player catcher) {
+        this.runner = runner;
+        this.catcher = catcher;
+
+        this.runnerPosition = new Position((float) Math.random(), (float) Math.random());
+        this.catcherPosition = new Position((float) Math.random(), (float) Math.random());
+
+        this.runnerSpeed = new Vector(0,0);
+        this.catcherSpeed = new Vector(0,0);
+    }
 
     public void tick() {
         Vector runnerMove = runner.getMove(runnerPosition, runnerSpeed, catcherPosition, catcherSpeed);
@@ -23,5 +36,7 @@ public class GameController {
 
         catcherSpeed.add(catcherMove);
         catcherPosition.move(catcherSpeed);
+
+        //TODO call frontend
     }
 }
