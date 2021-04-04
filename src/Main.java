ed5e48218ef917cc41ae4783992a4ade71f85423
Thomas Tieber <Thomas.Tieber@cloudflight.io>
2021-04-04 17:47:16 +0200

    make frontendcontroler react to player

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 import frontend.FrontendController;
+import player.Player;
+import utils.Position;
 
 public class Main {
     public static void main(String[] args) {
-        FrontendController frontendController = new FrontendController();
+//        Player catcher = new Player(new Position(200, 200));
+//        Player runner = new Player(new Position(300, 200));
+//        FrontendController frontendController = new FrontendController(catcher, runner);
+
+        System.out.println("Du Schneckli!");
     }
 }
--- src/frontend/FrontendController.java
+++ src/frontend/FrontendController.java
@@ -1,38 +1,51 @@
 package frontend;
 
+import player.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class FrontendController extends JPanel {
-    static int width = 25;
-    static int height = 25;
-
-    int x;
-    int y;
-    float vx;
-    float vy;
+    private static final int PLAYER_SIZE = 20;
+    private static final int FRAME_WIDTH = 500;
+    private static final int FRAME_HEIGHT = 500;
 
     private JFrame frame;
 
-    public FrontendController() {
-        x = 200;
-        y = 200;
-        vx = 10;
-        vy = 10;
+    private Player catcher;
+    private Player runner;
+
+    public FrontendController(Player catcher, Player runner) {
+        this.catcher = catcher;
+        this.runner = runner;
 
         frame = new JFrame("Catch Me!");
-
-        frame.setSize(500, 500);
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setContentPane(this);
         frame.setResizable(false);
+        frame.setContentPane(this);
         frame.setVisible(true);
     }
 
     @Override
     public void paint(Graphics g) {
         g.clearRect(0, 0, this.getWidth(), this.getHeight());
-        g.fillOval(x, y, width, height);
+
+        drawCatcher(g);
+        drawRunner(g);
+    }
+
+    private void drawCatcher(Graphics g) {
+        drawPlayer(g, catcher, Color.RED);
+    }
+
+    private void drawRunner(Graphics g) {
+        drawPlayer(g, runner, Color.BLUE);
+    }
+
+    private void drawPlayer(Graphics g, Player player, Color red) {
+        g.setColor(red);
+        g.fillOval((int) player.position.x, (int) player.position.y, PLAYER_SIZE, PLAYER_SIZE);
     }
 }
