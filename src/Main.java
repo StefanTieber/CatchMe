d75c79d328515ad7d0814355de521c826e699e87
panzi46 <panzi2003@gmail.com>
2021-04-04 13:24:10 +0200

    Test für  GIT mit Eclipse.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo du Schneckli!");
+        System.out.println("Hallo du Schneckli! Scheinbar funktioniert GIT auch mit Eclipse!");
     }
 }
